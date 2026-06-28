78aca4457431f4173703bfbca6a41e3dcb39ee1c
Dev Patel <devp78101@gmail.com>
2026-06-28 12:51:41 +0530

    Fix middleware order

--- Backend/server.js
+++ Backend/server.js
@@ -21,13 +21,15 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(errorMiddlware);
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/contact', contactRoute);
 app.use('/api/application', applicationRoute);
+
+app.use(errorMiddlware);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is runnign on ${process.env.PORT}`);
